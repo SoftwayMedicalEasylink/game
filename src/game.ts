64157a96d7e3ccf8fde89b85a3cb0addc95fbefc
Marwane Kalam-Alami <marwane@kalam-alami.net>
2020-02-24 12:39:34 +0100

    Fix Github pages caching

--- src/game.ts
+++ src/game.ts
@@ -2,15 +2,10 @@ import "phaser";
 import { MainScene } from "./scenes/main-scene";
 import { OtherScene } from "./scenes/other-scene";
 
-// when the page is loaded, create our game instance
-window.addEventListener("load", createGame);
-
-function createGame() {
-  const game = new Phaser.Game({
-    width: 800,
-    height: 600,
-    type: Phaser.AUTO,
-    parent: "game",
-    scene: [ MainScene, OtherScene ]
-  });
-}
+new Phaser.Game({
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: [ MainScene, OtherScene ]
+});
