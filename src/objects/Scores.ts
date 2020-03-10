6968f32497942eada79863bcbdc2d85e292b7d9f
Niskko <niskko.yt@gmail.com>
2020-03-10 12:49:19 +0100

    #16 score dans son propre GameObject

---
+++ src/objects/Scores.ts
@@ -0,0 +1,109 @@
+import { SnakeScene } from '../scenes/snake-scene';
+
+export class Scores extends Phaser.GameObjects.Container {
+    snakeScene: SnakeScene;
+    score = 0;
+    scoreText;
+
+    constructor(snakeScene: SnakeScene) {
+        super(snakeScene);
+        this.snakeScene = snakeScene;
+    }
+
+    public ShowScore() {
+        this.scoreText = this.snakeScene.add.text(16, 514, 'Score: ' + this.score, { fontSize: '32px', fill: '#000' });
+    };
+    public addPoint() {
+        this.score += 1;
+        this.snakeScene.Ncapsule -= 1;
+        this.scoreText.setText('Score: ' + this.score)
+    };
+    public BonusScore() {
+        if (this.score === 1) {
+            this.snakeScene.Speed = 250;
+            if (this.snakeScene.timerEvent.delay !== this.snakeScene.Speed) {
+              this.snakeScene.timerEvent = this.snakeScene.timerEvent.reset({
+                delay: this.snakeScene.Speed,
+                loop: true,
+                callback: this.snakeScene.moveSnake,
+                callbackScope: this.snakeScene
+              });
+            }
+          };
+          if (this.score === 5) {
+            this.snakeScene.chess.setVisible(false);
+            this.snakeScene.Speed = 175;
+            if (this.snakeScene.timerEvent.delay !== this.snakeScene.Speed) {
+              this.snakeScene.timerEvent = this.snakeScene.timerEvent.reset({
+                delay: this.snakeScene.Speed,
+                loop: true,
+                callback: this.snakeScene.moveSnake,
+                callbackScope: this.snakeScene
+              });
+            }
+          };
+          if (this.score === 15) {
+            this.snakeScene.chess.setVisible(true);
+            this.snakeScene.Speed = 100;
+            if (this.snakeScene.timerEvent.delay !== this.snakeScene.Speed) {
+              this.snakeScene.timerEvent = this.snakeScene.timerEvent.reset({
+                delay: this.snakeScene.Speed,
+                loop: true,
+                callback: this.snakeScene.moveSnake,
+                callbackScope: this.snakeScene
+              });
+            }
+          };
+          if (this.score === 25) {
+            this.snakeScene.Speed = 125;
+            if (this.snakeScene.timerEvent.delay !== this.snakeScene.Speed) {
+              this.snakeScene.timerEvent = this.snakeScene.timerEvent.reset({
+                delay: this.snakeScene.Speed,
+                loop: true,
+                callback: this.snakeScene.moveSnake,
+                callbackScope: this.snakeScene
+              });
+            }
+          };
+          if (this.score >= 50 && this.score < 100) {
+            this.snakeScene.chess.setTint(0x252525)
+            this.snakeScene.player.setTint(0x666666)
+            this.snakeScene.Speed = 125;
+            if (this.snakeScene.timerEvent.delay !== this.snakeScene.Speed) {
+              this.snakeScene.timerEvent = this.snakeScene.timerEvent.reset({
+                delay: this.snakeScene.Speed,
+                loop: true,
+                callback: this.snakeScene.moveSnake,
+                callbackScope: this.snakeScene
+              });
+            }
+          };
+          if (this.score >= 100 && this.score < 102) {
+            this.snakeScene.chess.setTint(this.snakeScene.color.random(50).color)
+            this.snakeScene.player.setTint(this.snakeScene.color.random(50).color)
+            this.snakeScene.Speed = 30;
+            if (this.snakeScene.timerEvent.delay !== this.snakeScene.Speed) {
+              this.snakeScene.timerEvent = this.snakeScene.timerEvent.reset({
+                delay: this.snakeScene.Speed,
+                loop: true,
+                callback: this.snakeScene.moveSnake,
+                callbackScope: this.snakeScene
+              });
+            }
+          };
+          if (this.score === 102) {
+            this.snakeScene.chess.setTint();
+            this.snakeScene.player.setTint();
+            this.snakeScene.Speed = 60;
+            if (this.snakeScene.timerEvent.delay !== this.snakeScene.Speed) {
+              this.snakeScene.timerEvent = this.snakeScene.timerEvent.reset({
+                delay: this.snakeScene.Speed,
+                loop: true,
+                callback: this.snakeScene.moveSnake,
+                callbackScope: this.snakeScene
+              });
+            }
+          };
+    };
+}
+
--- src/scenes/snake-scene.ts
+++ src/scenes/snake-scene.ts
@@ -1,4 +1,5 @@
 import { GameOver } from "../objects/game-over";
+import { Scores } from "../objects/Scores";
 
 export class SnakeScene extends Phaser.Scene {
   color = new Phaser.Display.Color();
@@ -6,8 +7,6 @@ export class SnakeScene extends Phaser.Scene {
   tile = 32;
   player: Phaser.Physics.Arcade.Sprite;
   cursors;
-  score = 0;
-  scoreText;
   timerEvent: Phaser.Time.TimerEvent;
   Speed = 500;
   velocityX = 0;
@@ -23,11 +22,12 @@ export class SnakeScene extends Phaser.Scene {
   SKey: Phaser.Input.Keyboard.Key;
   DKey: Phaser.Input.Keyboard.Key;
   gameOver: GameOver;
+  Scores: Scores;
 
   constructor() {
     super({ key: 'SnakeScene' });
   }
-
+  
   preload(): void {
     this.load.image('chess', "./assets/images/chess.png");
     this.load.image('ground', "./assets/images/ground.png");
@@ -40,9 +40,10 @@ export class SnakeScene extends Phaser.Scene {
     this.DKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D );
     
     this.gameOver = new GameOver(this);
+    this.Scores = new Scores(this);
     this.gameOver.preload();
   }
-
+  
   create(): void {
     this.chess = this.add.image(0, 0, 'chess').setOrigin(0);
     var Walls = this.physics.add.staticGroup();
@@ -60,27 +61,25 @@ export class SnakeScene extends Phaser.Scene {
     this.physics.add.collider(this.player, Walls, this.collides(this.player, Walls));
     this.player.setBounce(0);
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.scoreText = this.add.text(16, 514, 'Score: ' + this.score, { fontSize: '32px', fill: '#000' });
-
+    this.Scores.ShowScore();
+    
     this.timerEvent = this.time.addEvent({
       delay: this.Speed,
       loop: true,
       callback: this.moveSnake,
       callbackScope: this
     });
-
+    
     this.Ncapsule = 1;
     const capsule = this.physics.add.sprite(this.tile * 11, this.tile * 8, 'capsule').setOrigin(0);
-          capsule.scale = 0.25;
-          this.physics.add.overlap(this.player, capsule, this.collectCapsule());
+    capsule.scale = 0.25;
+    this.physics.add.overlap(this.player, capsule, this.collectCapsule());
   };
 
   collectCapsule(): ArcadePhysicsCallback {
     return (player: Phaser.Physics.Arcade.Sprite, capsule: Phaser.Physics.Arcade.Sprite) => {
       capsule.disableBody(true, true);
-      this.score += 1;
-      this.Ncapsule -= 1;
-      this.scoreText.setText('Score: ' + this.score)
+      this.Scores.addPoint()
     }
   }
   
@@ -97,12 +96,15 @@ export class SnakeScene extends Phaser.Scene {
         this.player.disableBody(true, true);
         this.player.x = this.tile;
         this.player.y = this.tile * 8;
+        this.velocityX = 0;
+        this.velocityY = 0;
         this.chess.setVisible(true);
         this.chess.setTint(0xFFB5B5);
       }
       this.gameOver.update();
       return;
     }
+    this.Scores.BonusScore();
     if (this.Ncapsule == 0) {
       function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
@@ -116,92 +118,6 @@ export class SnakeScene extends Phaser.Scene {
           this.physics.add.overlap(this.player, this.capsule, this.collectCapsule());
         };
       };
-    };
-    if (this.score === 1) {
-      this.chess.setVisible(true);
-      this.Speed = 250;
-      if (this.timerEvent.delay !== this.Speed) {
-        this.timerEvent = this.timerEvent.reset({
-          delay: this.Speed,
-          loop: true,
-          callback: this.moveSnake,
-          callbackScope: this
-        });
-      }
-    };
-    if (this.score === 5) {
-      this.chess.setVisible(false);
-      this.Speed = 175;
-      if (this.timerEvent.delay !== this.Speed) {
-        this.timerEvent = this.timerEvent.reset({
-          delay: this.Speed,
-          loop: true,
-          callback: this.moveSnake,
-          callbackScope: this
-        });
-      }
-    };
-    if (this.score === 15) {
-      this.chess.setVisible(true);
-      this.Speed = 100;
-      if (this.timerEvent.delay !== this.Speed) {
-        this.timerEvent = this.timerEvent.reset({
-          delay: this.Speed,
-          loop: true,
-          callback: this.moveSnake,
-          callbackScope: this
-        });
-      }
-    };
-    if (this.score === 25) {
-      this.Speed = 125;
-      if (this.timerEvent.delay !== this.Speed) {
-        this.timerEvent = this.timerEvent.reset({
-          delay: this.Speed,
-          loop: true,
-          callback: this.moveSnake,
-          callbackScope: this
-        });
-      }
-    };
-    if (this.score >= 50 && this.score < 100) {
-      this.chess.setTint(0x252525)
-      this.player.setTint(0x666666)
-      this.Speed = 125;
-      if (this.timerEvent.delay !== this.Speed) {
-        this.timerEvent = this.timerEvent.reset({
-          delay: this.Speed,
-          loop: true,
-          callback: this.moveSnake,
-          callbackScope: this
-        });
-      }
-    };
-    if (this.score >= 100 && this.score < 102) {
-      this.chess.setTint(this.color.random(50).color)
-      this.player.setTint(this.color.random(50).color)
-      this.Speed = 30;
-      if (this.timerEvent.delay !== this.Speed) {
-        this.timerEvent = this.timerEvent.reset({
-          delay: this.Speed,
-          loop: true,
-          callback: this.moveSnake,
-          callbackScope: this
-        });
-      }
-    };
-    if (this.score === 102) {
-      this.chess.setTint();
-      this.player.setTint();
-      this.Speed = 60;
-      if (this.timerEvent.delay !== this.Speed) {
-        this.timerEvent = this.timerEvent.reset({
-          delay: this.Speed,
-          loop: true,
-          callback: this.moveSnake,
-          callbackScope: this
-        });
-      }
     };
     if (this.cursors.left.isDown || this.QKey.isDown) {
       this.velocityX = -this.VELOCITY;
